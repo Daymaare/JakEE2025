a34becc626da85b80eb0b4c3bf8f86ff22a25189
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-02-27 13:05:29 +0100

    add Book class and code for BookApplication and BookResource

--- src/main/java/bookapp/BookApplication.java
+++ src/main/java/bookapp/BookApplication.java
@@ -2,9 +2,10 @@ package bookapp;
 
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("api")
-public class BookApplication {
+public class BookApplication extends Application {
     //Needed to enable Jakarta REST and specify path
 
 }
--- src/main/java/bookapp/BookResource.java
+++ src/main/java/bookapp/BookResource.java
@@ -6,13 +6,31 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Path("books")
 public class BookResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String firstCreate() {
-        return "First Test";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book firstCreate() {
+        return new Book("Boken som finns",123);
+
+    }
+
+    @GET
+    @Path("many")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Books manyCreate() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Boken som finns",123));
+        books.add(new Book("Boken som inte finns",456));
+        return new Books(books,2);
+    }
+
+    public record Books(List<Book> books, int totalBooks) {
+
     }
 
 }
