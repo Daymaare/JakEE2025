4839f55be722c6084a8ba8c497e5e827386d1ba0
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-03-05 11:50:17 +0100

    Change to GameResponse.java

--- src/main/java/gameapp/dto/GameResponse.java
+++ src/main/java/gameapp/dto/GameResponse.java
@@ -1,6 +1,7 @@
 package gameapp.dto;
 
 
+import gameapp.entity.Game;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +10,30 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class GameResponse {
+    private Long id;
     private String title;
     private String developer;
     private String description;
     private LocalDate releaseDate;
     private String upc;
 
-    public GameResponse(String title, String developer, String description, LocalDate releaseDate, String upc) {
+    public GameResponse(Long id,String title, String developer, String description, LocalDate releaseDate, String upc) {
+        this.id = id;
         this.title = title;
         this.developer = developer;
         this.description = description;
         this.releaseDate = releaseDate;
         this.upc = upc;
 
+    }
+
+    public static GameResponse map(Game game) {
+        return new GameResponse(
+                game.getId(),
+                game.getTitle(),
+                game.getDeveloper(),
+                game.getDescription(),
+                game.getReleaseDate().toLocalDate(),
+                game.getUpc() );
     }
 }
