5db3307992b8ed64f81dcd5da3e87e4aa720f0ff
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-03-05 10:05:18 +0100

    change from SEQUENCE to IDENTITY

--- src/main/java/gameapp/entity/Game.java
+++ src/main/java/gameapp/entity/Game.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class Game {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
