18aa846d9ba9f8aaa051fe9a40a4afc888984b8f
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-03-10 14:06:48 +0100

    add loggers

--- src/main/java/gameapp/GameResource.java
+++ src/main/java/gameapp/GameResource.java
@@ -10,11 +10,13 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.inject.Inject;
+import lombok.extern.java.Log;
 
 import java.util.List;
 import java.util.Objects;
 
 @Path("games")
+@Log
 public class GameResource {
 
     private GameRepository repository;
@@ -32,6 +34,7 @@ public class GameResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<GameResponse> getGames() {
+        log.info("Getting all games...");
         return repository.findAll()
                 .map(GameMapper::map)
                 .filter(Objects::nonNull)
@@ -43,6 +46,7 @@ public class GameResource {
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public GameResponse getOneGame(@PathParam("id") Long id) {
+        log.info("Getting game with ID " + id + "...");
         return repository.findById(id)
                 .map(GameMapper::map)
                 .orElseThrow(() -> new NotFound("Game with ID " + id + " not found"));
@@ -53,6 +57,7 @@ public class GameResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response createNewGame(CreateGame game) {
         if (game == null) {
+            log.info("Game cannot be null");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Game cannot be null")
                     .build();
@@ -60,6 +65,7 @@ public class GameResource {
         Game newGame = GameMapper.map(game);
         newGame = repository.insert(newGame);
         GameResponse response = new GameResponse(newGame);
+        log.info("Created new game: " + response);
         return Response.status(Response.Status.CREATED)
                 .header("Location", "/api/games/" + newGame.getId())
                 .entity(response)
@@ -73,20 +79,19 @@ public class GameResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response createNewGames(List<CreateGame> games) {
         if (games == null || games.isEmpty()) {
+            log.info("Game list cannot be null or empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Game list cannot be null or empty")
                     .build();
         }
-
         List<Game> createdGames = games.stream()
                 .map(GameMapper::map)
                 .map(repository::insert)
                 .toList();
-
         List<GameResponse> responses = createdGames.stream()
                 .map(GameMapper::map)
                 .toList();
-
+        log.info("Created games: " + responses);
         return Response.status(Response.Status.CREATED)
                 .entity(responses)
                 .build();
