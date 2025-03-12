bc05659010d9656d906814d7ebb29bab447e74df
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-03-12 13:51:14 +0100

    add findByTitleAndDeveloper

--- src/main/java/gameapp/GameRepository.java
+++ src/main/java/gameapp/GameRepository.java
@@ -2,6 +2,7 @@ package gameapp;
 
 import gameapp.entity.Game;
 import jakarta.data.repository.CrudRepository;
+import jakarta.data.repository.Find;
 import jakarta.data.repository.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
+
+    @Find
+    Optional<Game> findByTitleAndDeveloper(String title, String developer);
 }
