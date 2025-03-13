e0c8f499162dd2be8743531fe93ea6479f3c0b1e
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-03-13 13:20:05 +0100

    add @Query findByReleaseDateGreaterThan and change from Optional to List

--- src/main/java/gameapp/persistence/GameRepository.java
+++ src/main/java/gameapp/persistence/GameRepository.java
@@ -3,8 +3,12 @@ package gameapp.persistence;
 import gameapp.entity.Game;
 import jakarta.data.repository.CrudRepository;
 import jakarta.data.repository.Find;
+import jakarta.data.repository.Query;
 import jakarta.data.repository.Repository;
+import org.hibernate.type.descriptor.jdbc.internal.JdbcLiteralFormatterArray;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -12,12 +16,14 @@ import java.util.Optional;
 public interface GameRepository extends CrudRepository<Game, Long> {
 
     @Find
-    Optional<Game> findByTitleAndDeveloper(String title, String developer);
+    List<Game> findByTitleAndDeveloper(String title, String developer);
 
     @Find
-    Optional<Game> findByDeveloper(String developer);
+    List<Game> findByDeveloper(String developer);
 
     @Find
     Optional<Game> findByTitle(String title);
 
+    @Query("select g from Game g where releaseDate > :releaseDate")
+    List<Game> findByReleaseDateGreaterThan(LocalDate releaseDate);
 }
